e55b2aff80c5b4524e73891b06e7b8fef204413a
Szymon Słoniowski <karol2016pl@gmail.com>
2023-03-02 19:50:51 +0100

    Beta of the program + adding photos

--- Change.py
+++ Change.py
@@ -1,23 +1,38 @@
 import os
 import ctypes
+import time
+import itertools
 
-def set_wallpaper():
+def set_wallpaper(wallpaper_dir, selected_wallpaper):
     SPI_SETDESKWALLPAPER = 20
     ctypes.windll.user32.SystemParametersInfoW(SPI_SETDESKWALLPAPER, 0, os.path.join(wallpaper_dir, selected_wallpaper), 0)
     
-def get_wallpapers():
-    program_dir = os.path.dirname(os.path.abspath(__file__)) # uzyskaj ścieżkę do folderu programu
-    wallpaper_dir = os.path.join(program_dir, "tapety") # scieżka do folderu z tapetami
-    wallpapers = os.listdir(wallpaper_dir)# pobierz listę plików z folderu z tapetami
-    selected_wallpaper = wallpapers[wallpaper_number - 1]# wybierz tapetę o podanym numerze
+def get_wallpapers(wallpaper_number):
+    program_dir = os.path.dirname(os.path.abspath(__file__)) # get the path to the program folder
+    wallpaper_dir = os.path.join(program_dir, "tapety") # path to the wallpaper folder (where program.py is located)
+    wallpapers = os.listdir(wallpaper_dir)# get list of files from wallpapers folder
+    selected_wallpaper = wallpapers[wallpaper_number - 1]# select the wallpaper with the given number
+    return wallpaper_dir, selected_wallpaper
 
-wallpaper_number = int(input("Podaj numer tapety: ")) # pobierz numer tapety od użytkownika
-get_wallpapers()
-set_wallpaper()
+def clear_screen():
+    os.system('cls' if os.name == 'nt' else 'clear')#clear cmd depending on operatin sys. ( windows or unix-based)
 
+def loading_animation(): #Little animation for drip <3
+    dots = itertools.cycle([".", "..", "..."])
+    counter = 0
+    while counter < 3:  # Break after 3 dots
+        print(f"Loading{next(dots)}", end="\r")
+        time.sleep(0.5)
+        counter += 1
+            
+def all_in():
+    wallpaper_number = int(input("Podaj numer tapety: ")) # pobierz numer tapety od użytkownika
+    wallpaper_dir, selected_wallpaper = get_wallpapers(wallpaper_number)
+    loading_animation()
+    clear_screen()
+    print("// Wallpaper Changed \\\\ \n Have a nice Day <3")
+    set_wallpaper(wallpaper_dir, selected_wallpaper)
+    time.sleep(1)
+           
 
-
-
-
-
-
+all_in()
