89bb95a27906bbe4b60c50bdb5539f6f5d3bda55
Szymon Słoniowski <karol2016pl@gmail.com>
2023-03-04 09:19:17 +0100

    Bug fixed
    Program set the wallpaper as temporary (after reboot wallpaper was missing)

--- Change.py
+++ Change.py
@@ -3,22 +3,24 @@ import ctypes
 import time
 import itertools
 import subprocess
+import random
 
 
 def set_wallpaper(wallpaper_dir, selected_wallpaper):
+    # Ustawia tapetę jako tapetę systemową
     SPI_SETDESKWALLPAPER = 20
     ctypes.windll.user32.SystemParametersInfoW(
-        SPI_SETDESKWALLPAPER, 0, os.path.join(wallpaper_dir, selected_wallpaper), 0
+        SPI_SETDESKWALLPAPER, 0, os.path.join(wallpaper_dir, selected_wallpaper), 3
     )
+    time.sleep(1)
 
 
 def get_wallpapers(wallpaper_number):
     program_dir = os.path.dirname(
         os.path.abspath(__file__)
     )  # get the path to the program folder
-    wallpaper_dir = os.path.join(
-        program_dir, "tapety"
-    )  # path to the wallpaper folder (where program.py is located)
+    wallpaper_dir = os.path.join(program_dir, "Wallpapers")
+    # path to the wallpaper folder (where program.py is located)
     wallpapers = os.listdir(wallpaper_dir)  # get list of files from wallpapers folder
     selected_wallpaper = wallpapers[
         wallpaper_number - 1
@@ -39,12 +41,27 @@ def loading_animation():  # Little animation for drip <3
         print(f"Loading{next(dots)}", end="\r")
         time.sleep(0.5)
         counter += 1
+    clear_screen()
+    print("// Wallpaper Changed \\\\ \n Have a nice Day <3")
 
 
 def show_wallpapers():
     program_dir = os.path.dirname(os.path.abspath(__file__))
     wallpaper_dir = os.path.join(program_dir, "tapety")
     subprocess.Popen(f'explorer "{wallpaper_dir}"')
+
+
+def get_random_wallpaper():  # takes random wallpaper
+    program_dir = os.path.dirname(os.path.abspath(__file__))
+    wallpaper_dir = os.path.join(program_dir, "Wallpapers")
+    wallpaper_files = [
+        f for f in os.listdir(wallpaper_dir) if f.endswith(".jpg")
+    ]  # lista plików z rozszerzeniem .jpg w folderze
+    num_wallpapers = len(wallpaper_files)  # liczba plików z tapetami
+    random_index = random.randint(0, num_wallpapers - 1)
+    # wylosuj indeks jednej z tapet
+    selected_wallpaper = wallpaper_files[random_index]  # wybrana tapeta
+    return wallpaper_dir, selected_wallpaper
 
 
 def all_in():
@@ -68,27 +85,31 @@ def all_in():
                 "==========\nYou can enter the things below:\n   rand - chooses random wallpaper\n   show - opens a file_window with wallpapers to watch\n   day - chooses wallpaper that is good on the day (for the eyes)\n   night - chooses wallpaper that is good in the night (for the eyes)\n   party - chooses one of my favorite wallpapers <3\n============"
             )
             i += 1
-        elif user_input.lower() == "rand":  # random
-            print("rand")
+        elif user_input.lower() == "rand":
+            loading_animation()  # random
+            wallpaper_dir, selected_wallpaper = get_random_wallpaper()
+            set_wallpaper(wallpaper_dir, selected_wallpaper)
+
+            break
         elif user_input.lower() == "day":  # zimne kolory lub random
             print("day")
+
             break
         elif user_input.lower() == "night":
             # ciep kolory zolty pomarancz czerwony ciemny
             print("night")
+
             break
         elif user_input.lower() == "party":  # ulubiona tapeta
             print("party")
+
             break
         else:
             try:  # wybieranie numeru
                 wallpaper_number = int(user_input)
                 wallpaper_dir, selected_wallpaper = get_wallpapers(wallpaper_number)
                 loading_animation()
-                clear_screen()
-                print("// Wallpaper Changed \\\\ \n Have a nice Day <3")
                 set_wallpaper(wallpaper_dir, selected_wallpaper)
-                time.sleep(1)
                 break
             except ValueError:
                 print("\n==============\nZłe dane podałeś debilu\n==============\n")
@@ -97,4 +118,4 @@ def all_in():
 all_in()
 # To Do:
 # Add options for different wallpapers based on time of day or color scheme
-# Allow users to select random wallpaper / night / day / nr
+# Allow users to select wallpaper / night / day / nr
