9743f7fd585b340864150906f6093f3c029cd8fa
Szymon Słoniowski <karol2016pl@gmail.com>
2023-08-05 12:39:38 +0200

    Update Change_Wallpaper.py
    selecting folder from screen resolution
    using the arrows

--- Change_Wallpaper.py
+++ Change_Wallpaper.py
@@ -1,5 +1,11 @@
 import os
 from screeninfo import get_monitors
+import inquirer
+
+def get_screen_resolutions():
+    monitors = get_monitors()
+    resolutions = [f"Screen [{index + 1}] | {monitor.width}x{monitor.height}" for index, monitor in enumerate(monitors)]
+    return resolutions
 
 def get_resolution_folders():
     current_directory = os.path.dirname(os.path.abspath(__file__))
@@ -7,21 +13,30 @@ def get_resolution_folders():
     resolution_folders = [folder for folder in folders if 'x' in folder]
     return resolution_folders
 
-def get_screen_resolutions():
-    monitors = get_monitors()
-    resolutions = [f"Screen [{index + 1}] | {monitor.width}x{monitor.height}" for index, monitor in enumerate(monitors)]
-    return resolutions
+def choose_resolution_folder():
+    questions = [
+        inquirer.List('folder',
+                      choices=get_resolution_folders(),
+                      ),
+    ]
+    answers = inquirer.prompt(questions)
+    return answers['folder']
 
 
+
+# -----------------------------------------------------------------------------------------------------
 print("Witaj w wallpaperchanger, Wybierz folder z rodzielczością w której są Tapety do twojego Ekranu")
 
 print("\nRozdzielczości ekranów na tym komputerze:")
 for resolution in get_screen_resolutions():
     print(resolution)
 
-resolution_folders = get_resolution_folders()
-print("\nDostępne foldery z rozdzielczościami:\n")
-for folder in resolution_folders:
-    print(" "+folder)
+# resolution_folders = get_resolution_folders()
+# print("\nDostępne foldery z rozdzielczościami:\n")
+# for folder in resolution_folders:
+#     print(" "+folder)
+print("\n------------------------------\n")
+chosen_folder = choose_resolution_folder()
+print(f"------------------------------\nWybrano folder: {chosen_folder}")
 
 input("\nWcisnij enter aby zakonczyć")
